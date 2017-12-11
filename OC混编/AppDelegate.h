0bf2bd0f6a605054f9a2ab376e7b86ea7bfe74f3
罗嗣鸿 <apple@appledemacbook-pro.local>
2017-12-11 13:55:53 +0800

    Initial Commit

---
+++ OC混编/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC混编
+//
+//  Created by apple on 11/12/17.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
